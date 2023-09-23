3c5c522f7f70cc50e88def1b0b53e9ececb6fdcc
Azimbek <usmanovaz2001@gmail.com>
2023-09-23 11:36:11 +0600

    add docker

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            $table->string('pin')->unique();
+            $table->string('nickname')->unique();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
